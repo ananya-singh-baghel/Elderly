f5d4c1bd9ea9013382d764d39d8e9b498e92dcdc
Ananya <76189053+ananya-singh-baghel@users.noreply.github.com>
2023-05-21 20:59:59 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
-String twilio_sid = "AC1bd9e9cec09eec2f16516cddc9b51ac6";
-String auth_token = "9a2b4af9b9175c71bdba2f5f4f9ed6b8";
+String twilio_sid = "add sid";
+String auth_token = "add token";
 String mobile = "+12565872797";
 String mobile2 = "+918989840594";
-String places_api_key = "AIzaSyAAvWoW8fwXH_T70Nj8QlmNAdSTT5lOxbU";
+String places_api_key = "add places api key";
