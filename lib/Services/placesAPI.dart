c7334f875b8e7c72ba3fd8ae96e08f1622ca857d
Ananya <76189053+ananya-singh-baghel@users.noreply.github.com>
2023-05-21 21:01:39 +0530

    Update placesAPI.dart

--- lib/Services/placesAPI.dart
+++ lib/Services/placesAPI.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class NearYouAPI {
   Future<SearchResult?> getResult() async {
     final Uri url = Uri.parse(
-        "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyAAvWoW8fwXH_T70Nj8QlmNAdSTT5lOxbU&location=12.9692,79.1559&query=hospital&type=health&radius=2000");
+        "https://maps.googleapis.com/maps/api/place/textsearch/json?key="add key",79.1559&query=hospital&type=health&radius=2000");
     final response = await http.get(url);
     if (response.statusCode == 200) {
       return searchResultFromJson(response.body);
